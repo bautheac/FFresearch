9b0f28001ff55c9db5aeb00644e4a223f4e209f5
bautheac <o.bautheac@gmail.com>
2018-09-24 15:14:49 +0200

    Add breakspoints, portfolios and variables datasets

--- R/data.r
+++ R/data.r
@@ -1,22 +1,172 @@
-#' Fama/French research factors
+#' Fama/French variable breakpoints
 #'
-#' @description Fama/French research factors historical data. The time series dates back to 1926 with data for the momentum factor.
+#' @description Variable breakpoints time series. The variable breakpoints are
+#'   used to construct the Fama/French portfolios and factors.
 #'
-#' @format A data.table with variables:
+#' @format A \linkS4class{data.table} with variables:
 #' \itemize{
-#'   \item{Region}{Geographical market considered for factor construction.}
-#'   \item{Frequency}{Factor return frequency (day, month, year).}
-#'   \item{Period}{Period over which factor returns are calculated.}
-#'   \item{Mkt-Rf}{Market return minus risk free rate.}
-#'   \item{Rf}{Risk free rate of return.}
-#'   \item{Mkt}{Market return.}
-#'   \item{SMB}{Small minus big factor return.}
-#'   \item{HML}{High minus low factor return.}
-#'   \item{RMW}{Robust minus weak factor return.}
-#'   \item{CMA}{Conservative minus aggressive factor return.}
-#'   \item{Mom}{Momentum factor return.}
+#'   \item{variable: Fama/French variable name. See the "\link{variables}" dataset in this
+#'   package as well as \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/variable_definitions.html}
+#'   for details.}
+#'   \item{frequency: variable observation frequency.}
+#'   \item{percentile: percentile breakpoint. "# negative" and "# positive" count the number of negative and
+#'   positive occurences for a given period repectively.}
+#'   \item{period: observation period.}
+#'   \item{value: corresponding variable breakpoint value.}
 #' }
 #'
 #' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
 #'
-"fffactors"
+#' @importFrom data.table data.table
+#'
+"breakpoints"
+
+
+
+#' Fama/French research factors
+#'
+#' @description Return and level (risk free rate) time series for the Fama/French
+#'   asset pricing factor.
+#'
+#' @format A \linkS4class{data.table} with variables:
+#' \itemize{
+#'   \item{region: geographical market considered for factor construction.}
+#'   \item{frequency: factor return frequency (day, month, year).}
+#'   \item{period: period over which factor returns are calculated or risk free rate level is observed.}
+#'   \item{factor: Fama/French factor}
+#'   \item{value: corresponding return (factors) or level (risk free rate) observation.}
+#' }
+#'
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
+#'
+#' @importFrom data.table data.table
+#'
+"factors"
+
+
+#' Fama/French univariate sorts
+#'
+#' @description Return time series for Fama/French portfolios formed on
+#'   single variable sorts.
+#'
+#' @format A \linkS4class{data.table} with variables:
+#' \itemize{
+#'   \item{region: geographical market considered for factor construction.}
+#'   \item{frequency: field observation frequency.}
+#'   \item{sort variable: sort variable used for portfolio construction.}
+#'   \item{diviend: Y (included) or N (excluded).}
+#'   \item{weights: portoflio weighting method.}
+#'   \item{portfolio: corresponding portoflio.}
+#'   \item{field: variable observed for given portfolio.}
+#'   \item{period: observation period.}
+#'   \item{value: corresponding field value.}
+#' }
+#'
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
+#'
+#' @importFrom data.table data.table
+#'
+"portfolios_univariate"
+
+
+#' Fama/French bivariate sorts
+#'
+#' @description Return time series for Fama/French portfolios formed on
+#'   sorting on two variables.
+#'
+#' @format A \linkS4class{data.table} with variables:
+#' \itemize{
+#'   \item{region: geographical market considered for factor construction.}
+#'   \item{frequency: field observation frequency.}
+#'   \item{sort variable 1: first sort variable used for portfolio construction.}
+#'   \item{sort variable 2: second sort variable used for portfolio construction.}
+#'   \item{diviend: Y (included) or N (excluded).}
+#'   \item{weights: portoflio weighting method.}
+#'   \item{portfolio: corresponding portoflio.}
+#'   \item{field: variable observed for given portfolio.}
+#'   \item{period: observation period.}
+#'   \item{value: corresponding field value.}
+#' }
+#'
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
+#'
+#' @importFrom data.table data.table
+#'
+"portfolios_bivariate"
+
+
+
+#' Fama/French three-way sorts
+#'
+#' @description Return time series for Fama/French portfolios formed on
+#'   sorting on three variables.
+#'
+#' @format A \linkS4class{data.table} with variables:
+#' \itemize{
+#'   \item{region: geographical market considered for factor construction.}
+#'   \item{frequency: field observation frequency.}
+#'   \item{sort variable 1: first sort variable used for portfolio construction.}
+#'   \item{sort variable 2: second sort variable used for portfolio construction.}
+#'   \item{sort variable 3: third sort variable used for portfolio construction.}
+#'   \item{diviend: Y (included) or N (excluded).}
+#'   \item{weights: portoflio weighting method.}
+#'   \item{portfolio: corresponding portoflio.}
+#'   \item{field: variable observed for given portfolio.}
+#'   \item{period: observation period.}
+#'   \item{value: corresponding field value.}
+#' }
+#'
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
+#'
+#' @importFrom data.table data.table
+#'
+"portfolios_trivariate"
+
+
+
+
+#' Fama/French industry portfolios
+#'
+#' @description Return time series for Fama/French industry portfolios.
+#'
+#' @format A \linkS4class{data.table} with variables:
+#' \itemize{
+#'   \item{region: geographical market considered for factor construction.}
+#'   \item{frequency: field observation frequency.}
+#'   \item{diviend: Y (included) or N (excluded).}
+#'   \item{weights: portoflio weighting method.}
+#'   \item{portfolio: corresponding portoflio.}
+#'   \item{field: variable observed for given portfolio.}
+#'   \item{period: observation period.}
+#'   \item{value: corresponding field value.}
+#' }
+#'
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
+#'
+#' @importFrom data.table data.table
+#'
+"portfolios_industries"
+
+
+
+
+#' Fama/French variables
+#'
+#' @description Helper dataset. Provides details on Fama/French variables,
+#'   including contruction methods
+#'
+#' @format A \link[tibble]{tibble} with variables:
+#' \itemize{
+#'   \item{name: variable name.}
+#'   \item{symbol: symbol used to refer to a given variable in the various datasets
+#'   contained in the package.}
+#'   \item{description: variable descriptive elements including contruction methods.
+#'   See\url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/variable_definitions.html}
+#'   for more details.}
+#' }
+#'
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html}.
+#'
+#' @importFrom tibble tibble
+#'
+"variables"
--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-ffresearch
-==========
 
 [![Travis-CI Build Status](https://travis-ci.org/bautheac/ffresearch.svg?branch=master)](https://travis-ci.org/bautheac/ffresearch)
 [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/bautheac/ffresearch?branch=master&svg=true)](https://ci.appveyor.com/project/bautheac/ffresearch)
 [![lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 
-Fama/French research data conveniently packaged for consumption by R users.
+# FFresearch
+
+Fama/French research data conveniently packaged for consumption by R users. The data is pulled directly from Kenneth French's online [data library](http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html). Size concerns limit the data history to the last ten year; 
+the full time series are available from the author upon request.
+
+## Installation
+
+Install the development version from [github](https://github.com/bautheac/FFresearch/) with:
+
+``` r
+devtools::install_github(repo = "FFresearch", username = "bautheac")
+```
+
+## Example
+
+Classic Fama & French asset pricing factors
+
+``` r
+library(FFresearch)
+data(list = c("factors"), package = "FFresearch", envir = environment())
+```
